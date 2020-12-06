62fe871e71c8e773237d13489cc5756b6652f565
khanraquib <57582855+khanraquib@users.noreply.github.com>
2020-12-07 00:54:12 +0530

    name added

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+
+var name1 = prompt("Enter player 1 name ");
+var name2 = prompt("Enter player 2 name ");
+
+document.querySelector("p").innerHTML = name1;
+
+
+document.getElementById("naa").innerHTML = name2;
+
 var randomnumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randonDiceImage = "dice" + randomnumber1 + ".png";
@@ -19,12 +28,15 @@ var image1 = document.querySelectorAll("img")[1];
 
 image1.setAttribute("src", randomImageSource2);
 
+// document.querySelector("p").innerHTML = "Player 2 wins";
+
+
 if (randomnumber1 > randomnumber2) {
-    document.querySelector("h1").innerHTML = "Player 1 wins";
+    document.querySelector("h1").innerHTML = name1 + " wins";
 
 }
 else if (randomnumber2 >randomnumber1) {
-    document.querySelector("h1").innerHTML = "Player 2 wins";
+    document.querySelector("h1").innerHTML = name2 + " wins";
 }
 else {
     document.querySelector("h1").innerHTML = "Draw";
